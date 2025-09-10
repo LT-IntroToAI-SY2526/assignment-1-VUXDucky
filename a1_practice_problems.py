5362cff7072d14ba83f09d61e3affb6606a507b3
Lukas Leahu <lleahu@cps.edu>
2025-09-10 11:22:55 -0500

    Finished sum

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -84,7 +84,10 @@ def sum_list(lst: List[int]) -> int:
     Returns:
         the sum of the passed in list
     """
-    raise NotImplementedError("sum_list")
+    total = 0
+    for num in lst:
+        total += num 
+    return total
 
 
 def mean(lst: List[int]) -> float:
@@ -145,6 +148,7 @@ if __name__ == "__main__":
     assert factorial(0) == 1, "factorial of 0 failed"
     assert every_other([1, 2, 3, 4, 5]) == [1, 3, 5], "every_other of [1,2,3,4,5] failed"
     assert sum_list([1, 2, 3]) == 6, "sum_list of [1,2,3] failed"
+    assert sum_list([54, 23, 9, 17]) == 103, "sum_list of [54,23,3,9,17] failed"
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
     assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
 
